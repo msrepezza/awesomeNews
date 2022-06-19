8493f8e0de09a92ddfd6ee8183f2429955c93c36
Soledad Repezza <maria.soledad.repezza@dirmod.com>
2022-06-19 17:40:05 -0300

    se agregan iconos

--- src/components/CardArticle.js
+++ src/components/CardArticle.js
@@ -18,7 +18,14 @@ export const CardArticle = ({ data }) => {
 
           <div className='p-2 divide-y divide-neutral'>
             <div className='flex items-center justify-between h-12'>
-              <div className='bodyDescription'> {data.category} </div>
+              <div className='bodyDescription'> 
+                <span className='bodyDescription'>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                  </svg>
+                  {data.category} 
+                </span>
+              </div>
               <div>
                 <span><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -30,7 +37,12 @@ export const CardArticle = ({ data }) => {
 
             <div>
               <div className='flex items-center justify-between h-12' >
-                <p className='bodyDescription'> { formatDate(data.createdAt) } </p>
+                <span className='bodyDescription'>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                  </svg>
+                  { formatDate(data.createdAt) } 
+                </span>
                 <Link to={`/article/${data.id}`}><button className='bg-purple rounded px-2 text-xs text-white h-8 w-18'>Read More</button></Link>
               </div>
             </div>
--- src/pages/articles/index.js
+++ src/pages/articles/index.js
@@ -24,7 +24,13 @@ export const Article = () => {
         <div className='grid box-content w-600 justify-items-center'>
           <div>
             <div className='grid grid-cols-3 justify-between'>
-              <div className='bodyDescription text-center'> { formatDate(article.createdAt) } </div>
+              <div className='bodyDescription text-center'> 
+                <span>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                  </svg>
+                </span> <span> { formatDate(article.createdAt) } </span>
+              </div>
               <div className='bodyDescription text-center'> 
                 <span>
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
@@ -32,11 +38,18 @@ export const Article = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                   </svg></span> <span> {article.views} </span>
               </div>
-              <div className='bodyDescription text-center'> {article.category} </div>
+              <div className='bodyDescription text-center'>
+                <span className='bodyDescription'>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                  </svg>
+                  {article.category} 
+                </span>
+              </div>
             </div>
           </div>
 
-          <div>
+          <div className='m-2'>
             <img src={article.imageUrl} alt='Imagen del articulo'/>
           </div>
         </div>
